3de3761c3cc3ccd34332021dbed1b581d4500423
Primqd <franklinvchen@outlook.com>
2025-06-27 23:09:57 -0700

    made button go up on interact (idk man)

--- Assets/Scripts/Interactibles/Button.cs
+++ Assets/Scripts/Interactibles/Button.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Button : Interactible // inherit from interactble
 {
+
     // interaction code
+    void Start()
+    {
+        // No need to assign transform; it's inherited from MonoBehaviour
+    }
+
     protected override void Interact()
     {
         Debug.Log("Interacted with " + gameObject.name);
+        transform.Translate(Vector3.up * 100f * Time.deltaTime);
     }
 }
